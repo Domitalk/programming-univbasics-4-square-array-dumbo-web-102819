f3f1ea9cec89418009c936ad6c2e2447df3febf5
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 00:29:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0
   new_numbers = []
   while counter < numbers.length do
-    new_numbers.push(numbers[counter] * numbers[couunter])
+    new_numbers.push(numbers[counter] * numbers[counter])
     counter += 1
   end
   return new_numbers
