698eca3823570cc1a7e5bc1b01dbc686e25a085e
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-17 00:27:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,21 @@
 def square_array(numbers)
-  counter = 0
-  number1 = 0
+  counter = 0 
   new_numbers = []
   while counter < numbers.length do
-    temp_number = numbers[counter]
-    number1 = (temp_number * temp_number)
-    new_numbers.push(number1)
+    new_numbers.push(numbers[counter] * numbers[couunter])
     counter += 1
   end
-  return new_numbers
+  new_numbers
+
+  # Solution 1
+  # counter = 0
+  # number1 = 0
+  # new_numbers = []
+  # while counter < numbers.length do
+  #   temp_number = numbers[counter]
+  #   number1 = (temp_number * temp_number)
+  #   new_numbers.push(number1)
+  #   counter += 1
+  # end
+  # return new_numbers
 end
